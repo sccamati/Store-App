eb77b0ccca331649dfe76ed958f25f3055258c2d
sccamati <64529630+sccamati@users.noreply.github.com>
2021-06-23 16:49:46 +0200

    added delete product

--- Store-App/ClientApp/src/app/product/product.component.ts
+++ Store-App/ClientApp/src/app/product/product.component.ts
@@ -8,9 +8,17 @@ import { HttpClient } from '@angular/common/http';
 export class ProductDataComponent {
   public products: Product[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
     http.get<Product[]>(baseUrl + 'api/products').subscribe(result => {
       this.products = result;
+    }, error => console.error(error));
+  }
+
+  onDelete(id: number) {
+    this.http.delete<Product[]>(this.baseUrl + 'api/products/' + id).subscribe(result => {
+      let room = this.products.find(x => x.id === id);
+      let index = this.products.indexOf(room, 0);
+      this.products.splice(index, 1);
     }, error => console.error(error));
   }
 }
